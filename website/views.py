931c09fb64bf7d814b1ed55f0160fe539f0f3a99
Haroon Shahid <108454007+HaroonShahid1@users.noreply.github.com>
2023-09-25 12:15:05 +0500

    Initial commit

---
+++ website/views.py
@@ -0,0 +1,24 @@
+import requests
+from django.shortcuts import render
+import plotly.express as px
+import pandas as pd
+
+def fetch_covid_data_and_display_chart(request):
+    api_url = 'https://disease.sh/v3/covid-19/countries'  # COVID-19 data API endpoint
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # Process the data and create a DataFrame
+        df = pd.DataFrame(data)
+
+        # Create a chart using Plotly
+        fig = px.bar(df, x='country', y='cases', title='COVID-19 Cases by Country')
+        chart_div = fig.to_html(full_html=False)
+
+        context = {'chart_div': chart_div}
+    else:
+        context = {'chart_div': None}  # Handle API request error
+
+    return render(request, 'charting/chart.html', context)
